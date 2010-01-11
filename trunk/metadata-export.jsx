7604f6b89cc1d340515ae44cfb15e342ee1214ce
T.Dmitry <T.Dmitry@b898e7e6-e179-11dd-8fb1-9960f2a117f8>
2010-01-11 00:00:06 +0000

    typo fix

--- trunk/metadata-export.jsx
+++ trunk/metadata-export.jsx
@@ -6,7 +6,7 @@
 /// Version: 0.1
 ///////////////////
 /*
-    Copyright (C) 2009-2010  Tyzhnenko Dmitry
+    Copyright (C) 2010  Tyzhnenko Dmitry
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
