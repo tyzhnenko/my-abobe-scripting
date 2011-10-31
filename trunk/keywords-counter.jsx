d4603558394500df27fd360494d0ba5cf122a98c
T.Dmitry <T.Dmitry@b898e7e6-e179-11dd-8fb1-9960f2a117f8>
2011-10-31 15:25:35 +0000

    change head on jsx files

--- trunk/keywords-counter.jsx
+++ trunk/keywords-counter.jsx
@@ -1,25 +1,22 @@
-///////////////////
-/// Name: Keyword counter
-/// Description: This script show tatal keywords in file and have many use for for microstocker fuctional.
-/// Author: Tyzhnenko Dmitry
-/// E-mail: t.dmitry@gmail.com
-/// Version: 0.72
-///////////////////
 /*
-    Copyright (C) 2009-2010  Tyzhnenko Dmitry
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+.--------------------------------------------------------------------------.
+|    Software: Keyword counter                                             |
+|     Version: 0.72                                                        |
+|        Site: http://code.google.com/p/my-abobe-scripting/                |
+| Description: This script show tatal keywords in file and have            |
+|                               many use for for microstocker fuctional.   |
+| -------------------------------------------------------------------------|
+|     Admin: Tyzhnenko Dmitry (project admininistrator)                    |
+|    Author: Tyzhnenko Dmitry t.dmitry@gmail.com                           |
+|   Founder: Tyzhnenko Dmitry (original founder)                           |
+| Copyright (c) 2009-2011, Tyzhnenko Dmitry                                |
+| -------------------------------------------------------------------------|
+|   License: Distributed under the General Public License v3 (GPLv3)       |
+|            http://www.gnu.org/licenses/gpl.html                          |
+| This program is distributed in the hope that it will be useful - WITHOUT |
+| ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or    |
+| FITNESS FOR A PARTICULAR PURPOSE.                                        |
+'--------------------------------------------------------------------------'
 */
 /*
     Changelog
